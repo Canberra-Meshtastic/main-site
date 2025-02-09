5e150c09b36d999b0ba95a3c4b684f2c72c3ac88
Ella Hinschen <ella@ellahin.com>
2025-02-09 11:40:23 +1100

    fixed link

--- src/app/nodes/page.tsx
+++ src/app/nodes/page.tsx
@@ -1,18 +1,20 @@
 import { CommunityNodes } from "@/components/communitynodes"
-import { Link } from "lucide-react"
-  
-  export default function Nodes() {
-    return (
-      <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-3xl text-center">
-            <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
-              Community Nodes
-            </h2>
-            <p className="mt-2 text-lg/8 text-gray-600">All of these nodes have been created and maintained by Canberra Meshtastic Comunity.</p>
-          </div>
-          <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {CommunityNodes.map((node) => (
+import Link from "next/link"
+
+
+export default function Nodes() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-3xl text-center">
+          <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
+            Community Nodes
+          </h2>
+          <p className="mt-2 text-lg/8 text-gray-600">All of these nodes have been created and maintained by Canberra Meshtastic Comunity.</p>
+        </div>
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {CommunityNodes.map((node) => (
+            <Link href={`/nodes/${node.id}`}>
               <div key={node.id} className="flex flex-col items-start justify-between">
                 <div className="relative w-full">
                   <img
@@ -25,18 +27,19 @@ import { Link } from "lucide-react"
                 <div className="max-w-xl">
                   <div className="group relative">
                     <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                      <Link href={`/nodes/${node.id}`}>
-                        <span className="absolute inset-0" />
-                        {node.name}
-                      </Link>
+
+                      <span className="absolute inset-0" />
+                      {node.name}
+
                     </h3>
                     <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{node.description}</p>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            </Link>
+          ))}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
